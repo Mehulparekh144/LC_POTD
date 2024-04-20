16eaa4db0e3465d180c68fc0b07326ede53cca44
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-04-20 11:47:40 -0400

    Time: 6 ms (82.13%), Space: 60.9 MB (53.61%) - LeetHub

--- 2103-find-all-groups-of-farmland/2103-find-all-groups-of-farmland.java
+++ 2103-find-all-groups-of-farmland/2103-find-all-groups-of-farmland.java
@@ -1,18 +1,4 @@
 class Solution {
-    private void dfs(int i , int j , int[][] land , int[] path){
-        int m  = land.length;
-        int n = land[0].length;
-        if(i < 0 || j < 0 || i>= m || j >= n || land[i][j] == 0 ){
-            return;
-        }
-        path[2] = Math.max(path[2] , i);
-        path[3] = Math.max(path[3] , j);
-        land[i][j] = 0;
-        dfs(i+1 , j , land , path);
-        dfs(i-1 , j , land , path);
-        dfs(i , j-1 , land , path);
-        dfs(i , j+1 , land , path);
-    }
 
     public int[][] findFarmland(int[][] land) {
         int m = land.length;
@@ -21,12 +7,27 @@ class Solution {
 
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(land[i][j] == 1){
-                    int[] path = new int[4];
-                    path[0] = i;
-                    path[1] = j;
-                    dfs(i , j , land ,path);
-                    ans.add(path);
+                if(land[i][j] == 0) continue;
+
+                int j1 = j;
+                while(j1 < n  && land[i][j1] == 1){
+                    j1++;
+                }
+
+                int i1 = i;
+                while(i1 < m && land[i1][j] == 1){
+                    i1++;
+                }
+
+                j1 = j1 == 0 ? j1 : j1-1;
+                i1 = i1 == 0 ? i1 : i1-1;
+
+                ans.add(new int[]{i , j , i1 , j1});
+
+                for(int row = i ; row <= i1 ; row++){
+                    for(int col = j ; col <= j1 ; col++){
+                        land[row][col] = 0;
+                    }
                 }
             }
         }
