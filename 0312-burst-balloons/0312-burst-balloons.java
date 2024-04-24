21d9fa42426f1acc2ad79711c3eb68255dadbfd4
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-04-24 00:23:59 -0400

    Time: 76 ms (61.75%), Space: 42.3 MB (83.25%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,28 +1,29 @@
 class Solution {
-    int[][] dp;
-    private int burst(int i , int j , List<Integer> numsL){
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int maxCoins = Integer.MIN_VALUE;
-        for(int k = i ; k <= j ; k++){
-            int coins = numsL.get(i-1)*numsL.get(k)*numsL.get(j+1) + burst(i , k-1 , numsL) + burst(k+1 , j , numsL);   
-            if(coins > maxCoins){
-                maxCoins = coins;
-            }
-        }
-        return dp[i][j] = maxCoins;
-    }
     public int maxCoins(int[] nums) {
-        dp = new int[nums.length + 2][nums.length + 2];
-        for(int[] row : dp){
-            Arrays.fill(row , -1);
-        }
+        int[][] dp = new int[nums.length + 2][nums.length + 2];
         List<Integer> numsL = new ArrayList<>();
         for(int num : nums){
             numsL.add(num);
         }
         numsL.add(1);
         numsL.add(0 , 1);
-        return burst(1 , nums.length , numsL);
+
+        for(int i = nums.length ; i >= 1 ; i--){
+            for(int j = 1 ; j <= nums.length ; j++){
+                if(i > j){
+                    continue;
+                }
+                int maxCoins = Integer.MIN_VALUE;
+                for(int k = i ; k <= j ; k++){
+                    int coins = numsL.get(i-1) * numsL.get(k) * numsL.get(j+1) + dp[i][k-1] + dp[k+1][j];
+                    if(coins > maxCoins){
+                        maxCoins = coins;
+                    }
+                }
+
+                dp[i][j] = maxCoins;
+            }
+        }
+        return dp[1][nums.length];
     }
 }
