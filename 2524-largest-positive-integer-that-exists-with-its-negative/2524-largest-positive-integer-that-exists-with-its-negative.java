97cee5da1de3c6cd3f8bbef109590c1d98ae05b9
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-05-01 22:36:57 -0400

    Time: 5 ms (84.11%), Space: 45 MB (30.47%) - LeetHub

--- 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.java
@@ -5,13 +5,32 @@ class Solution {
 
        while(j >= 0){
         int target = nums[j];
-        for(int num : nums){
-            if(target*-1 == num) return target;
-        }
+        if(binarySearch(nums , target*-1) != Integer.MAX_VALUE) return target;
         j--;
        }
 
        return -1;
+    }
 
+    public int binarySearch(int[] nums , int target){
+        int l = 0 ;
+        int r = nums.length - 1;
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                return target;
+            }
+
+            else if(nums[mid] < target){
+                l = mid + 1;
+            }
+
+            else{
+                r = mid - 1;
+            }
+        }
+
+        return Integer.MAX_VALUE;
     }
 }
