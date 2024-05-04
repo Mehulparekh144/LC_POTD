0841c573ad3b89a47c5f0792648f13d2c5259a11
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-05-04 15:41:16 -0400

    Time: 5 ms (76.13%), Space: 45.7 MB (49.69%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+    List<List<Integer>> adj = new ArrayList<>();
+    for(int i = 0 ; i < numCourses ; i++){
+      adj.add(new ArrayList<>());
+    }
+
+    int[] indegree = new int[numCourses];
+    for(int[] preq : prerequisites){
+      adj.get(preq[1]).add(preq[0]);
+      indegree[preq[0]]++;
+    }
+
+    Queue<Integer> q = new LinkedList<>();
+
+    for(int i = 0 ; i < numCourses ; i++){
+      if(indegree[i] == 0){
+        q.offer(i);
+      }
+    }
+
+    List<Integer> order = new ArrayList<>();
+    while(!q.isEmpty()){
+      int course = q.poll();
+      order.add(course);
+
+      for(int child : adj.get(course)){
+        indegree[child]--;
+        if(indegree[child] == 0){
+          q.offer(child);
+        }
+      }
+    }
+
+    if(order.size() != numCourses) return new int[0]; 
+    
+    int[] res = new int[numCourses];
+    for (int i = 0; i < order.size() ; i++){
+      res[i] = order.get(i);
+    }
+    
+    return res;
+    }
+
+}
