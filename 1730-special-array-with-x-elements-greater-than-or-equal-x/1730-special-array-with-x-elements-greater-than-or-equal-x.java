59fcb93c16e917a96b2513531597ecd9b7ea0e35
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-05-27 18:44:04 -0400

    Time: 2 ms (47.95%), Space: 41.2 MB (52.97%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,20 +1,34 @@
 class Solution {
     public int specialArray(int[] nums) {
-        int x = nums.length;
-        int res = 0;
-        for(int i = 0 ; i <= x ; i++){
-          int count = 0;
+        Arrays.sort(nums);
+        int n = nums.length;
 
-          for(int num : nums){
-            if(num >= i) count++;
-          }
-
-          if(count == i){
-            res += count;
-          }
-          
+        for(int x = 0; x <= n ; x++){
+          // Find idx of number just greater than equal to x
+          int idx = lowerBound(nums , x);
+          if(idx == -1) continue;
+          int count = n - idx;
+          if(count == x) return x;
         }
 
-        return res == 0 ? -1 : res;
+        return -1;
+    }
+
+    private int lowerBound(int[] nums , int x){
+      int l = 0;
+      int r = nums.length - 1;
+      int idx = -1;
+      while(l <= r){
+        int mid = (l+r)/2;
+
+        if(nums[mid] >= x){
+          idx = mid;
+          r = mid - 1;
+        } else{
+          l = mid + 1;
+        }
+      }
+
+      return idx;
     }
 }
