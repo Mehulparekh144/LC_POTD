a9f9974f34d2d0ebd6b4aff1e7e4c27e8088f31e
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-04 18:59:44 -0400

    Time: 5 ms (37.52%), Space: 42.6 MB (5.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,31 +1,32 @@
 class Solution {
-    List<String> result;
-    Map<String,String> map;
-    public void solve(int i , String digits ,String s){
-      if(s.length() == digits.length() ){
-        result.add(s);
+    public List<String> letterCombinations(String digits) {
+      Map<Character,String> map = new HashMap<>();
+      map.put('2' , "abc");
+      map.put('3' , "def");
+      map.put('4' , "ghi");
+      map.put('5' , "jkl");
+      map.put('6' , "mno");
+      map.put('7' , "pqrs");
+      map.put('8' , "tuv");
+      map.put('9' , "wxyz");
+
+      if(digits.length() == 0){
+        return new ArrayList<>();
+      }        
+      List<String> res = new ArrayList<>();
+      solve(0 , digits , "" , map , res);
+      return res;
+    }
+
+    private void solve(int i , String digits , String path, Map<Character,String> map , List<String> res){
+      if(path.length() == digits.length()){
+        res.add(path);
         return;
       }
-      for(char c : map.get(String.valueOf(digits.charAt(i))).toCharArray()){
-        solve(i + 1 , digits , s + c );
+
+      String letters = map.get(digits.charAt(i));
+      for(char l : letters.toCharArray()){
+        solve(i+1 , digits , path + l , map , res);
       }
-    }
-    
-    public List<String> letterCombinations(String digits) {
-      result = new ArrayList<>();
-      map = new HashMap<>();
-      map.put("2" , "abc");
-      map.put("3" , "def");
-      map.put("4" , "ghi");
-      map.put("5" , "jkl");
-      map.put("6" , "mno");
-      map.put("7" , "pqrs");
-      map.put("8" , "tuv");
-      map.put("9" , "wxyz");
-      if(digits.isEmpty()){
-        return result;
-      }
-      solve(0 , digits , "");
-      return result;
     }
 }
