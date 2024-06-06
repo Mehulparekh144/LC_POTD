c300c2ab644ba98c845e2839eb2749d73c26deba
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-06 17:23:13 -0400

    Time: 11 ms (91.07%), Space: 48.6 MB (34.04%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,40 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-      Map<Integer,Integer> map = new HashMap<>();
-
-      for(int num : nums){
-        map.put(num , map.getOrDefault(num , 0) + 1);
-      }  
-
-      PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[1] - a[1]);
-
-      for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-        maxHeap.offer(new int[]{entry.getKey() , entry.getValue()});
+  public int[] topKFrequent(int[] nums, int k) {
+  
+      Map<Integer, Integer> map = new HashMap<>();
+      for (int num : nums) {
+        map.put(num, map.getOrDefault(num, 0) + 1);
       }
-
-      int[] ans = new int[k];
-      for(int i = 0 ; i < k && !maxHeap.isEmpty() ; i++ ){
-        ans[i] = maxHeap.poll()[0];
+  
+      List<Integer>[] freq = new List[nums.length + 1];
+  
+      for (int i = 0; i < freq.length; i++) {
+        freq[i] = new ArrayList<>();
       }
-
-      return ans;
+      for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        freq[entry.getValue()].add(entry.getKey());
+      }
+    
+      List<Integer> ans = new ArrayList<>();
+      for (int i = freq.length - 1; i >= 0; i--) {
+        for (int j = 0; j < freq[i].size(); j++) {
+          ans.add(freq[i].get(j));
+          if (ans.size() == k) {
+            break;
+          }
+        }
+        if (ans.size() == k) {
+          break;
+        }
+      }
+  
+      int idx = 0;
+      int[] res = new int[ans.size()];
+      for (int num : ans) {
+        res[idx++] = num;
+      }
+  
+      return res;
+  
     }
 }
