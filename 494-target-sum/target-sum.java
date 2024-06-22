e551af069bbf78dcb2c975f387f2045867bb5bd0
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-21 23:01:54 -0400

    Time: 5 ms (87.00%) | Memory: 42.7 MB (54.88%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,19 +1,34 @@
 class Solution {
+    int[][] dp;
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        return solve(0 , nums , target);  
+        int sum = Arrays.stream(nums).sum();
+        if(sum-target < 0 || (sum-target)%2 != 0){
+            return 0;
+        }
+        dp = new int[n][(sum - target)/2 + 1];
+        for(int[] row : dp) Arrays.fill(row , -1);
+        return solve(n-1 , nums , (sum - target)/2);  
     }
 
     private int solve(int i , int[] nums , int target){
-        if(i == nums.length){
-            if(target == 0){
+        if(i == 0){
+            if(target == 0 && nums[0] == 0){
+                return 2;
+            } 
+            if(target == 0 || nums[0] == target){
                 return 1;
             }
             return 0;
         }
 
-        int plus = solve(i+1 , nums , target - nums[i]);
-        int minus = solve(i+1 , nums , target + nums[i]);
-        return plus + minus;
+        if(dp[i][target] != -1) return dp[i][target];
+
+        int np = solve(i-1 , nums , target);
+        int p = 0;
+        if(target >= nums[i]){
+            p = solve(i-1 , nums , target - nums[i]);
+        }
+        return dp[i][target] = p + np;
     }
 }
