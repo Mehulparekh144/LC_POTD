7785e73ca0e91a02348a0a38a600950e20ff8033
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-23 01:42:45 -0400

    Time: 5 ms (65.44%) | Memory: 45.4 MB (6.63%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,33 +1,32 @@
 class Solution {
-    int[][] dp;
-    public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        dp = new int[m][n];
-        for(int[] row : dp) Arrays.fill(row , -1);
-        return solve(m-1 , n-1 , word1 , word2);
-    }
+    public int minDistance(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
 
-    private int solve(int i , int j , String s , String t){
-        // j is left to be traversed. Whatever is left will be inserted
-        if(i < 0){
-            return j + 1;
+        for(int j = 0 ; j <= n ; j++){
+            dp[0][j] = j;
         } 
-        // if i is left and j is exhausted. That means all characters left in s will be deleted.
-        if(j < 0){
-            return i + 1;
+
+        for(int i = 0 ; i <= m ; i++){
+            dp[i][0] = i;
         }
 
-        if(dp[i][j] != -1) return dp[i][j];
+        for(int i = 1 ; i <= m ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                } else{
+                    int insert = 1 + dp[i][j-1];
+                    int delete = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+                    dp[i][j] = Math.min(insert , Math.min(replace , delete));
+                }
+            }
+        }
 
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = solve(i-1 , j-1 , s , t);
-        }
-        else{
-            int insert = 1 + solve(i , j - 1 , s , t);
-            int replace = 1 + solve(i-1 , j-1 , s , t);
-            int delete = 1 + solve(i-1 , j , s , t);
-            return dp[i][j] = Math.min(insert , Math.min(replace , delete));
-        }
+        return dp[m][n];
+
     }
+
 }
