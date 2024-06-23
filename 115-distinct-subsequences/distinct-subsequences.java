1f6b138b693f262b31f29e592ff707ac08058421
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-23 01:24:19 -0400

    Time: 16 ms (29.56%) | Memory: 49 MB (29.98%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,28 +1,28 @@
 class Solution {
-    int[][] dp;
+    
+
     public int numDistinct(String s, String t) {
-        dp = new int[s.length() + 1][t.length() + 1];
-        for(int[] row : dp) Arrays.fill(row , -1);
-        return solve(s.length() , t.length() , s , t);
-    }
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
 
-    private int solve(int i , int j , String s , String t){
-        if(j == 0){
-            return 1;
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 1;
         }
 
-        if(i == 0){
-            return 0;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                }
+                else{
+                 dp[i][j] = dp[i-1][j];
+                }
+            }
         }
 
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s.charAt(i-1) == t.charAt(j-1)){
-            return dp[i][j] = solve(i-1 , j-1 , s , t) + solve(i-1 , j ,s ,t);
-        } 
-
-        return dp[i][j] = solve(i-1 , j , s , t);
+        return dp[s.length()][t.length()];
     }
 
+    
 
 }
