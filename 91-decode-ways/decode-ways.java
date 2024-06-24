407377052860c6a4de98cbcafa75d8199a46857d
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-24 00:05:21 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (42.68%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,25 +1,29 @@
 class Solution {
     public int numDecodings(String s) {
-        int[] dp = new int[s.length() + 1];
+        int prev = 1;
+        int prev2 = 0;
+        int curr = 0;
 
-        dp[s.length()] = 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                curr = 0;
 
-        for(int i = s.length() - 1 ; i >= 0 ; i--){
-            if(s.charAt(i) == '0'){
-                continue;
+            } else {
+                int step1 = prev;
+                int step2 = 0;
+                if (i + 1 < s.length() && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7'))) {
+                    step2 = prev2;
+                }
+
+                curr = step1 + step2;
+
             }
+            prev2 = prev;
+            prev = curr;
 
-            int step1 = dp[i+1];
-            int step2 = 0;
-            if(i+1 < s.length() && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7')))
-            {
-                step2 = dp[i+2];
-            }
-
-            dp[i] = step1 + step2;
         }
 
-        return dp[0];
+        return curr;
 
     }
 }
