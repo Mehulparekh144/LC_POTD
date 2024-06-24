b9df3801a4697414ba9329a3ee470acbf4518dc4
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-24 01:06:11 -0400

    Time: 1 ms (83.08%) | Memory: 41.9 MB (32.68%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -2,50 +2,45 @@ class Solution {
     int m;
     int n;
     int l;
-    int[][][] dp;
+    int[][] dp;
+
     public boolean isInterleave(String s1, String s2, String s3) {
         m = s1.length();
         n = s2.length();
         l = s3.length();
-        dp = new int[m+1][n+1][l+1];
-        for(int[][] grid : dp){
-            for(int[] row : grid){
-                Arrays.fill(row , -1);
-            }
+        dp = new int[m + 1][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
         if (m + n != l) {
             return false;
         }
 
-        return solve(0, 0, 0, s1, s2, s3);
+        return solve(0, 0, s1, s2, s3);
     }
 
-    private boolean solve(int i, int j, int k, String s1, String s2, String s3) {
-        if (i == m && j == n && k == l) {
+    private boolean solve(int i, int j, String s1, String s2, String s3) {
+        if (i == m && j == n ) {
             return true;
         }
 
-        if (k >= l) {
-            return false;
-        }
-
-        if(dp[i][j][k] != -1){
-            return dp[i][j][k] == 1;
+        if (dp[i][j] != -1) {
+            return dp[i][j] == 1;
         }
 
         boolean result = false;
-        if (i < m && s1.charAt(i) == s3.charAt(k)) {
-            result = solve(i + 1, j, k + 1, s1, s2, s3);
+        if (i < m && s1.charAt(i) == s3.charAt(i + j)) {
+            result = solve(i + 1, j, s1, s2, s3);
         }
 
         if (result)
             return true;
 
-        if (j < n && s2.charAt(j) == s3.charAt(k)) {
-            result = solve(i, j + 1, k + 1, s1, s2, s3);
+        if (j < n && s2.charAt(j) == s3.charAt(i + j)) {
+            result = solve(i, j + 1, s1, s2, s3);
         }
 
-        dp[i][j][k] = result ? 1 : 0;
+        dp[i][j] = result ? 1 : 0;
 
         return result;
     }
