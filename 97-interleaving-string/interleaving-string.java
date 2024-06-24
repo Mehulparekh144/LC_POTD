38cfd93b0238b20a08127601f093f46ca70194d8
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-24 01:12:18 -0400

    Time: 7 ms (28.77%) | Memory: 41.9 MB (32.68%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -2,46 +2,35 @@ class Solution {
     int m;
     int n;
     int l;
-    int[][] dp;
 
     public boolean isInterleave(String s1, String s2, String s3) {
         m = s1.length();
         n = s2.length();
         l = s3.length();
-        dp = new int[m + 1][n + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
         if (m + n != l) {
             return false;
         }
+        boolean[][] dp = new boolean[m + 1][n + 1];
 
-        return solve(0, 0, s1, s2, s3);
+        dp[m][n] = true;
+
+        for (int i = m; i >= 0; i--) {
+            for (int j = n; j >= 0; j--) {
+                boolean result = false;
+
+                if (i < m && s1.charAt(i) == s3.charAt(i + j)) {
+                    dp[i][j] = dp[i][j] || dp[i+1][j];
+                } 
+
+                if (j < n && s2.charAt(j) == s3.charAt(i + j)) {
+                    dp[i][j] = dp[i][j] || dp[i][j+1];
+                }
+
+            }
+        }
+
+        return dp[0][0];
     }
 
-    private boolean solve(int i, int j, String s1, String s2, String s3) {
-        if (i == m && j == n ) {
-            return true;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j] == 1;
-        }
-
-        boolean result = false;
-        if (i < m && s1.charAt(i) == s3.charAt(i + j)) {
-            result = solve(i + 1, j, s1, s2, s3);
-        }
-
-        if (result)
-            return true;
-
-        if (j < n && s2.charAt(j) == s3.charAt(i + j)) {
-            result = solve(i, j + 1, s1, s2, s3);
-        }
-
-        dp[i][j] = result ? 1 : 0;
-
-        return result;
-    }
+   
 }
