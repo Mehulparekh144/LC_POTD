83c9f19110435b13773a6254c1b44652a8f08810
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-25 19:45:17 -0400

    Time: 71 ms (64.47%) | Memory: 42 MB (97.93%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,31 +1,33 @@
 class Solution {
     int[][] dp;
-    public int maxCoins(int[] nums) {
-        dp = new int[nums.length + 1][nums.length + 1];
-        for(int[] row : dp) Arrays.fill(row , -1);
-        List<Integer> list = new ArrayList<>();
 
-        for(int num : nums) {
+    public int maxCoins(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        dp = new int[nums.length + 2][nums.length + 2];
+        for (int num : nums) {
             list.add(num);
         }
 
         list.addFirst(1);
         list.addLast(1);
 
-        return solve(1 , nums.length , list);
-    }
+        for (int i = nums.length; i >= 1; i--) {
+            for (int j = 1; j <= nums.length; j++) {
+                
+                if(i > j) continue;
 
-    private int solve(int i , int j , List<Integer> list){
-        if(i > j) return 0;
+                int max = Integer.MIN_VALUE;
+                for (int k = i; k <= j; k++) {
+                    int coins = list.get(i - 1) * list.get(k) * list.get(j + 1) + dp[i][k-1] + dp[k+1][j];
+                    max = Math.max(coins, max);
+                }
 
-        if(dp[i][j] != -1) return dp[i][j];
+                dp[i][j] = max;
 
-        int max = Integer.MIN_VALUE;
-        for(int k = i ; k <= j ; k++){
-            int coins = list.get(i-1) * list.get(k) * list.get(j+1) + solve(i , k-1 , list) + solve(k+1 , j , list);
-            max = Math.max(coins , max);
+            }
         }
 
-        return dp[i][j] = max;
+        return dp[1][nums.length];
     }
+
 }
