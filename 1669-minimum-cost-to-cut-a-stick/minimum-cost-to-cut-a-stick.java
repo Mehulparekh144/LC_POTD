da421c75a48ff05a02dab9968b7daf2786d3d107
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-25 19:21:30 -0400

    Time: 20 ms (70.76%) | Memory: 41.9 MB (94.35%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,31 +1,32 @@
 class Solution {
     int[][] dp;
+
     public int minCost(int n, int[] cuts) {
-        dp = new int[cuts.length + 1][cuts.length + 1];
-        for(int[] row : dp) {
-            Arrays.fill(row , -1);
-        }
+        dp = new int[cuts.length + 2][cuts.length + 2];
         List<Integer> cutsL = new ArrayList<>();
-        for(int cut : cuts) cutsL.add(cut);
+        for (int cut : cuts)
+            cutsL.add(cut);
 
         Collections.sort(cutsL);
-        cutsL.add(0 , 0);
+        cutsL.add(0, 0);
         cutsL.addLast(n);
 
-        return solve(1 , cuts.length , cutsL);
-    }
+        for (int i = cuts.length; i >= 1; i--) {
+            for (int j = 1; j <= cuts.length; j++) {
+                if (i > j) {
+                    continue;
+                }
 
-    private int solve(int i , int j , List<Integer> cutsL){
-        if(i > j) return 0;
+                int min = Integer.MAX_VALUE;
+                for (int k = i; k <= j; k++) {
+                    int cost = cutsL.get(j + 1) - cutsL.get(i - 1) + dp[i][k-1] + dp[k+1][j];
+                    min = Math.min(cost, min);
+                }
 
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int min = Integer.MAX_VALUE;
-        for(int k = i ; k <= j ; k++){
-            int cost = cutsL.get(j+1) - cutsL.get(i-1) + solve(i , k-1 , cutsL) + solve(k+1 , j , cutsL);
-            min = Math.min(cost , min);
+                dp[i][j] = min;
+            }
         }
 
-        return dp[i][j] = min;
+        return dp[1][cuts.length];
     }
-    }
+}
