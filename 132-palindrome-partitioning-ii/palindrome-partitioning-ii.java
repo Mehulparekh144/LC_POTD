9b62271c01c252d99f7b72571721500ca5976d19
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-06-26 17:47:16 -0400

    Time: 2474 ms (37.07%) | Memory: 41.1 MB (82.13%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,25 +1,23 @@
 class Solution {
-    int[] dp;
+
     public int minCut(String s) {
-        dp = new int[s.length()];
-        Arrays.fill(dp , -1);
-        return solve(0 , s) - 1;
-    }
+        int[] dp = new int[s.length() + 1];
 
-    private int solve(int i , String s){
-        if(i == s.length()) return 0;
 
-        if(dp[i] != -1) return dp[i];
-
+        for(int i = s.length() - 1 ; i >= 0 ; i--){
         int mini = Integer.MAX_VALUE;
-        for(int j = i ; j < s.length() ; j++){
-            if(isPalindrome(s , i , j)){
-                int cut = 1 + solve( j + 1 , s);
-                if(cut < mini) mini = cut;
+            for(int j = i ; j < s.length() ; j++ ){
+                if(isPalindrome(s , i , j)){
+                    int cut = 1 + dp[j+1];
+                    if(mini > cut) mini = cut;
+                }
             }
+
+            dp[i] = mini;
+
         }
 
-        return dp[i] = mini;
+        return dp[0] - 1;
     }
 
     private boolean isPalindrome(String s , int i , int j){
