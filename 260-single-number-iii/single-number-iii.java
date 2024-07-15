53404af00c8a1b0335b6cbc84b8d1aa9aa5f1678
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-07-15 16:20:22 -0400

    Time: 1 ms (99.97%) | Memory: 45.8 MB (10.07%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        // XOR All elements
+        int xor = 0;
+        for(int num : nums){
+          xor ^= num;
+        }
+
+        // Check which bit is set for xor;
+        int setBit = 0;
+        for(int i = 0 ; i < 32 ; i++){
+          if((xor & (1 << i)) != 0){
+            setBit = i;
+            break;
+          }
+        }
+
+        int groupA = 0;
+        int groupB = 0;
+
+        for(int num : nums){
+          if((num & (1 << setBit)) == 0){
+            groupA ^= num;
+          } else{
+            groupB ^= num;
+          }
+        }
+
+        return new int[]{groupA , groupB};
+    }
+}
