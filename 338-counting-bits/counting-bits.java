e8b36c4387d0708a1febc2b6dcdbdfb85426179b
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-07-14 20:04:48 -0400

    Time: 2 ms (92.85%) | Memory: 46.5 MB (39.16%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n+1];
+      int[] dp = new int[n+1];
+      int offset = 1;
 
-        for(int i = 0 ; i <= n ; i++){
-          ans[i] = setBits(i);
+      for(int i = 1 ; i <= n ; i++){
+        if(offset*2 == i){
+          offset = i;
         }
 
-        return ans;
-    }
-
-    private int setBits(int n){
-      int count = 0;
-
-      while(n > 1){
-        count += n & 1;
-        n >>= 1;
+        dp[i] = 1 + dp[i - offset];
       }
 
-      if(n == 1) count++;
-      return count;
+      return dp;
     }
+
 }
