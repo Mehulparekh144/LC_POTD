c26af3fc6d8643f305cd1c3b390f173d13eb2db3
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-02 18:35:06 -0400

    Time: 9 ms (62.93%) | Memory: 58.4 MB (86.87%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,29 +1,49 @@
 class Solution {
   int[] dp;
+
   public boolean validPartition(int[] nums) {
-    dp = new int[nums.length];
-    Arrays.fill(dp , -1);
-    return dfs(0, nums);
-  }
+    boolean[] dp = new boolean[nums.length + 1];
 
-  private boolean dfs(int i, int[] nums) {
-    if (i == nums.length)
-      return true;
-
-    if(dp[i] != -1) return dp[i] == 1;
-
-    boolean res = false;
+    dp[nums.length] = true;
+    for(int i = nums.length - 1 ; i >= 0 ; i--){
+      boolean res = false;
     if (i + 1 < nums.length && nums[i] == nums[i + 1]) {
-      res = dfs(i + 2, nums);
+      res = dp[i+2];
     }
 
     if (i + 2 < nums.length) {
       if ((nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) || (nums[i] + 1 == nums[i + 1] && nums[i + 1] == nums[i + 2] - 1)) {
-        res = res || dfs(i + 3, nums);
+        res = res || dp[i+3];
       }
     }
 
-    dp[i] = res ? 1 : 0;
-    return res;
+    dp[i] = res;
+
+    }
+
+    return dp[0];
   }
+
+  // private boolean dfs(int i, int[] nums) {
+  //   if (i == nums.length)
+  //     return true;
+
+  //   if (dp[i] != -1)
+  //     return dp[i] == 1;
+
+  //   boolean res = false;
+  //   if (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+  //     res = dfs(i + 2, nums);
+  //   }
+
+  //   if (i + 2 < nums.length) {
+  //     if ((nums[i] == nums[i + 1] && nums[i] == nums[i + 2])
+  //         || (nums[i] + 1 == nums[i + 1] && nums[i + 1] == nums[i + 2] - 1)) {
+  //       res = res || dfs(i + 3, nums);
+  //     }
+  //   }
+
+  //   dp[i] = res ? 1 : 0;
+  //   return res;
+  // }
 }
