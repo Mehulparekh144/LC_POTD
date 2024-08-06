46db747a3088d9fcb7f1c04eaf8f707e2a92f718
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-06 19:40:31 -0400

    Time: 13 ms (99.64%) | Memory: 42.6 MB (93.76%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,39 +1,42 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
+
+        Arrays.sort(nums);
+
         int[] dp = new int[n];
         Arrays.fill(dp , 1);
+
         int[] hash = new int[n];
+
         int maxi = -1;
         int idx = -1;
 
         for(int i = 0 ; i < n ; i++){
-            hash[i] = i;
-            for(int prev = 0 ; prev < i ; prev++){
-                if(nums[i]%nums[prev] == 0 && 1 + dp[prev] > dp[i]){
-                    dp[i] = 1 + dp[prev];
-                    hash[i] = prev;
-                }
-            }
+          hash[i] = i;
 
-            if(dp[i] > maxi){
-                maxi = dp[i];
-                idx = i;
+          for(int prev= 0  ; prev < i ; prev++){
+            if(nums[i]%nums[prev] == 0 && 1 + dp[prev] > dp[i]){
+              dp[i] = 1 + dp[prev];
+              hash[i] = prev;
             }
+          }
+
+          if(dp[i] > maxi){
+            maxi = dp[i];
+            idx = i;
+          }
+
         }
-
-        System.out.println(Arrays.toString(dp));
-
 
         List<Integer> res = new ArrayList<>();
-        res.add(nums[idx]);
-        
-        while(hash[idx] != idx){
-            idx = hash[idx];
-            res.add(nums[idx]);
+        res.addFirst(nums[idx]);
+
+        while(idx != hash[idx]){
+          idx = hash[idx];
+          res.addFirst(nums[idx]);
         }
-        Collections.reverse(res);
+
         return res;
     }
 }
