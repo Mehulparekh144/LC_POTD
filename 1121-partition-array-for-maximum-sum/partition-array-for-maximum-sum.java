a96c47dadd9aafbcadc31f537c22bff14678e3dc
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-06 19:24:16 -0400

    Time: 95 ms (5.07%) | Memory: 41.9 MB (56.95%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,32 +1,33 @@
 class Solution {
+    int[] dp;
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int[] dp = new int[arr.length + 1];
-
-        for(int i = arr.length - 1 ; i >= 0 ; i--){
-            int maxi = Integer.MIN_VALUE;
-            for(int j = i ; j < i + k && j < arr.length ; j++){
-                int sum = getSum(arr , i , j) + dp[j+1];
-                if(sum > maxi) maxi = sum;
-            }
-
-            dp[i] = maxi;
-        }
-
-        return dp[0];
+        dp = new int[arr.length + 1];
+        Arrays.fill(dp , -1);
+        return solve(0 , arr , k);
     }
 
-    
+    private int solve(int i , int[] arr , int k){
+      if(i == arr.length) return 0;
+
+      if(dp[i] != -1) return dp[i];
+
+      int maxi = Integer.MIN_VALUE;
+      for(int j = i ; j < i + k && j < arr.length ; j++){
+        int sum = getSum(arr , i , j) + solve(j+1 , arr , k);
+        maxi = Math.max(maxi , sum);
+      }
+
+      return dp[i] = maxi;
+    }
 
     private int getSum(int[] arr , int i , int j){
-        int maxi = Integer.MIN_VALUE;
-        for(int k = i ; k <= j ; k++){
-            if(arr[k] > maxi){
-                maxi = arr[k];
-            }
-        }
+      int maxi = Integer.MIN_VALUE;
 
-        int sum = maxi * (j-i + 1);
+      for(int k = i ; k <= j ; k++){
+        if(arr[k] > maxi) maxi = arr[k];
+      }
 
-        return sum;
+      int sum = maxi * (j-i+1);
+      return sum;
     }
 }
