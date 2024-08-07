b8fb4442a0fae3c01f55a90a2d042ab239c0865e
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-07 15:44:35 -0400

    Time: 11 ms (82.89%) | Memory: 56.7 MB (75.67%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -2,27 +2,31 @@ class Solution {
   int[] dp;
 
   public String stoneGameIII(int[] stoneValue) {
-    dp = new int[stoneValue.length + 1];
+    int prev1 = 0;
+    int prev2 = 0;
+    int prev3 = 0;
 
     for (int i = stoneValue.length - 1; i >= 0; i--) {
-      int pickOne = stoneValue[i] - dp[i + 1];
+      int pickOne = stoneValue[i] - prev1;
       int pickTwo = Integer.MIN_VALUE;
       int pickThree = Integer.MIN_VALUE;
 
       if (i + 2 <= stoneValue.length) {
-        pickTwo = stoneValue[i] + (i + 1 < stoneValue.length ? stoneValue[i + 1] : 0) - dp[i + 2];
+        pickTwo = stoneValue[i] + (i + 1 < stoneValue.length ? stoneValue[i + 1] : 0) -  prev2;
       }
 
       if (i + 3 <= stoneValue.length) {
         pickThree = stoneValue[i] + (i + 1 < stoneValue.length ? stoneValue[i + 1] : 0)
-            + (i + 2 < stoneValue.length ? stoneValue[i + 2] : 0) - dp[i + 3];
+            + (i + 2 < stoneValue.length ? stoneValue[i + 2] : 0) - prev3;
 
       }
-
-      dp[i] = Math.max(pickOne, Math.max(pickTwo, pickThree));
+      
+      prev3 = prev2;
+      prev2 = prev1;
+      prev1 = Math.max(pickOne, Math.max(pickTwo, pickThree));
     }
 
-    int res = dp[0];
+    int res = prev1;
     if (res > 0) {
       return "Alice";
     } else if (res < 0) {
