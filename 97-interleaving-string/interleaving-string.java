240d1972db5c049c6ebfc338559605cd699a815c
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-08 23:40:09 -0400

    Time: 7 ms (30.23%) | Memory: 41.6 MB (59.54%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,47 +1,51 @@
 class Solution {
-    int m ;
-    int n ;
-    int l ;
-    int[][][] dp;
+  int m;
+  int n;
+  int l;
+  boolean[][] dp;
 
-    public boolean isInterleave(String s1, String s2, String s3) {
-      m = s1.length();
-      n = s2.length();
-      l = s3.length();
+  public boolean isInterleave(String s1, String s2, String s3) {
+    m = s1.length();
+    n = s2.length();
+    l = s3.length();
 
-      dp = new int[m+1][n+1][l+1];
-      for(int[][] mat : dp){
-        for(int[] row : mat){
-          Arrays.fill(row , -1);
-        }
+    if(m + n != l) return false;
+
+    dp = new boolean[m + 1][n + 1];
+
+    dp[m][n] = true;
+
+
+    for (int i = m; i >= 0; i--) {
+      for (int j = n; j >= 0; j--) {
+
+          if (i < m && s1.charAt(i) == s3.charAt(i + j)) {
+            dp[i][j] = dp[i][j] || dp[i+1][j];
+          }
+
+          if (j < n && s2.charAt(j) == s3.charAt(i + j)) {
+            dp[i][j] = dp[i][j] || dp[i][j+1];
+          }
+
       }
-
-      return dfs(0 , 0 , 0 , s1 , s2 ,s3);    
     }
 
-    private boolean dfs(int i , int j , int k , String s1 , String s2 , String s3){
+    return dp[0][0];
+  }
 
-      if(i == m && j == n && k == l) return true;
+  // private boolean dfs(int i, int j, int k, String s1, String s2, String s3) {
 
-      if(k >= l) return false;
+  //   if (i == m && j == n && k == l)
+  //     return true;
 
-      if(dp[i][j][k] != -1) return dp[i][j][k] == 1;
+  //   if (k >= l)
+  //     return false;
 
+  //   if (dp[i][j][k] != -1)
+  //     return dp[i][j][k] == 1;
 
-      boolean res = false;
+  //   dp[i][j][k] = res == true ? 1 : 0;
 
-      if(i < m && s1.charAt(i) == s3.charAt(k)){
-        res = dfs(i+1 , j , k+1 , s1 , s2 ,s3);
-      }
-
-      if(res) return true;
-
-      if(j < n && s2.charAt(j) == s3.charAt(k)){
-        res = dfs(i , j+1 , k+1 , s1 , s2 , s3);
-      }
-
-      dp[i][j][k] = res == true ? 1 : 0;
-
-      return res;
-    }
+  //   return res;
+  // }
 }
