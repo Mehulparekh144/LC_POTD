1b20e94f75e61a7c40ab6d3d882f869d3c471ad1
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-09 14:05:22 -0400

    Time: 8 ms (52.98%) | Memory: 57.4 MB (51.72%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,34 +1,43 @@
 class Solution {
-    int[][] dp;
-    public int maximalSquare(char[][] matrix) {
-      int m = matrix.length;
-      int n = matrix[0].length;
-      int[] mat = new int[1];
-      dp = new int[m][n];
+  public int maximalSquare(char[][] matrix) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+    int max = 0;
+    int[][] dp = new int[m][n];
 
-      for(int[] row : dp) Arrays.fill(row , -1);
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (matrix[i][j] == '1') {
 
-      for(int i = 0 ; i < m ; i++){
-        for(int j = 0 ; j < n ; j++){
-          dfs(i , j , mat , matrix);
+          if (i == 0 || j == 0) {
+            dp[i][j] = 1;
+          } else {
+            int up = dp[i - 1][j];
+            int left = dp[i][j - 1];
+            int diag = dp[i - 1][j - 1];
+
+            dp[i][j] = Math.min(up, Math.min(left, diag)) + 1;
+          }
+          max = Math.max(dp[i][j], max);
         }
       }
-
-      return mat[0]*mat[0];
     }
 
-    private int  dfs(int i , int j , int[] mat , char[][] matrix){
-      if(i < 0 || j < 0 || matrix[i][j] == '0') return 0;
+    return max * max;
+  }
 
-      if(dp[i][j] != -1) return dp[i][j];
+  // private int dfs(int i , int j , int[] mat , char[][] matrix){
+  // if(i < 0 || j < 0 || matrix[i][j] == '0') return 0;
 
-      int up = dfs(i-1 , j , mat , matrix);
-      int left = dfs(i , j-1 , mat , matrix);
-      int diag = dfs(i-1 , j-1 , mat , matrix);
+  // if(dp[i][j] != -1) return dp[i][j];
 
-      int side = Math.min(up , Math.min(left , diag)) + 1;
+  // int up = dfs(i-1 , j , mat , matrix);
+  // int left = dfs(i , j-1 , mat , matrix);
+  // int diag = dfs(i-1 , j-1 , mat , matrix);
 
-      mat[0] = Math.max(mat[0] , side);
-      return dp[i][j] = side;    
-    }
+  // int side = Math.min(up , Math.min(left , diag)) + 1;
+
+  // mat[0] = Math.max(mat[0] , side);
+  // return dp[i][j] = side;
+  // }
 }
