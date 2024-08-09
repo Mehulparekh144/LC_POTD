2fda7e1111266941ca354876f89ae929409906a5
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-09 17:18:47 -0400

    Time: 23 ms (28.56%) | Memory: 41.5 MB (87.98%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,29 +1,44 @@
 class Solution {
   private static final int MOD = (int) 1e9 + 7;
-  private int[][] dp;
-    public int numRollsToTarget(int n, int k, int target) {
-       dp = new int[n+1][target + 1];
-       for(int[] row : dp) Arrays.fill(row , -1);
-       return solve(n , k , target); 
-    }
 
-    private int solve(int n , int k , int target){
-      if(target < 0) return 0;
-      if(n < 0) return 0;
+  public int numRollsToTarget(int n, int k, int target) {
+    int[][] dp = new int[n + 1][target + 1];
 
-      if(n == 0){
-        return target == 0 ? 1 : 0;
-      }
+    dp[0][0] = 1;
 
-      if(dp[n][target] != -1) return dp[n][target];
-
-      int ways = 0;
-
-      for(int i = 1 ; i <= k ; i++){
-          ways = (ways + solve(n-1 , k , target - i))%MOD;
+    for (int i = 1; i <= n; i++) {
+      for (int t = 0; t <= target; t++) {
+        for (int x = 1; x <= k; x++) {
+          if(t < x) continue;
+          dp[i][t] = (dp[i][t] + dp[i-1][t - x])%MOD;
+        }
 
       }
-
-      return dp[n][target] = ways;
     }
+
+    return dp[n][target];
+  }
+
+  // private int solve(int n, int k, int target) {
+  //   if (target < 0)
+  //     return 0;
+  //   if (n < 0)
+  //     return 0;
+
+  //   if (n == 0) {
+  //     return target == 0 ? 1 : 0;
+  //   }
+
+  //   if (dp[n][target] != -1)
+  //     return dp[n][target];
+
+  //   int ways = 0;
+
+  //   for (int i = 1; i <= k; i++) {
+  //     ways = (ways + solve(n - 1, k, target - i)) % MOD;
+
+  //   }
+
+  //   return dp[n][target] = ways;
+  // }
 }
