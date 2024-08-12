6cb6db5801a590172f5f65284d14f9b18860de2f
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-12 15:45:41 -0400

    Time: 36 ms (88.10%) | Memory: 58.8 MB (33.33%) - LeetSync

--- 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
+++ 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
@@ -1,25 +1,21 @@
+/**
+ * Pick largest candle 3 so now other sub problem will be (n-1)* dfs why n-1
+ * because since 3 is largest all others will be visible
+ */
 class Solution {
   private final static int MOD = (int) 1e9 + 7;
-  Long[][] dp;
-    public int rearrangeSticks(int n, int k) {
-      dp = new Long[n+1][k + 1];
-      return (int) solve(n , k );
+  public int rearrangeSticks(int n, int k) {
+    long[][] dp = new long[n + 1][k + 1];
+
+    dp[1][1] = 1;
+
+    for(int i = 2 ; i <= n ; i++){
+      for(int j = 1 ; j <= k ; j++){
+        dp[i][j] = dp[i-1][j-1] + (i-1)*dp[i-1][j];
+        dp[i][j] %= MOD;
+      }
     }
 
-    private long solve(int n , int k){
-      if(n == k) {
-        return 1;
-      }
-      if(n == 0 || k == 0){
-        return 0;
-      }
-
-      if(dp[n][k] != null) return dp[n][k];
-
-      long ways = solve(n-1 , k-1) + (n-1)* solve(n-1 , k);
-
-      dp[n][k] = ways%MOD;
-
-      return ways%MOD;
-    }
+    return (int) dp[n][k];
+  }
 }
