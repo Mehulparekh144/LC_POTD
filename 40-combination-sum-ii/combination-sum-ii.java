7abd8dbef7bb99c491b691731ae6d6be49af26fd
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-12 23:34:45 -0400

    Time: 3 ms (82.44%) | Memory: 43 MB (85.22%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,27 +1,27 @@
 class Solution {
+
+
   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
     Arrays.sort(candidates);
     List<List<Integer>> res = new ArrayList<>();
-    solve(0, candidates, target, res, new ArrayList<>());
+    dfs(0, candidates, target, new ArrayList<>() , res);
     return res;
   }
 
-  private void solve(int i, int[] nums, int target, List<List<Integer>> res, List<Integer> temp) {
-
+  private void dfs(int i, int[] candidates, int target, List<Integer> temp , List<List<Integer>> res) {
     if (target == 0) {
       res.add(new ArrayList<>(temp));
       return;
     }
 
-    for (int j = i; j < nums.length; j++) {
-      if(j > i && nums[j] == nums[j-1] ) continue;
+    for(int j = i ; j < candidates.length ; j++){
+      if(j > i && candidates[j] == candidates[j-1]) continue;
 
-      if (nums[j] <= target) {
-        temp.add(nums[j]);
-        solve(j+1, nums, target - nums[j], res, temp);
+      if(target >= candidates[j]){
+        temp.add(candidates[j]);
+        dfs(j+1 , candidates , target - candidates[j] , temp , res);
         temp.removeLast();
       }
     }
-
   }
 }
