e950cf73d6221cd08204f181fd53c26a79ba2e37
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-14 18:25:22 -0400

    Time: 11 ms (83.14%) | Memory: 45.3 MB (59.77%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.java
+++ 2808-painting-the-walls/painting-the-walls.java
@@ -1,25 +1,21 @@
 class Solution {
-  int[][] dp;
   public int paintWalls(int[] cost, int[] time) {
-    dp = new int[cost.length][cost.length + 1];
-    for(int[] row  : dp){
-      Arrays.fill(row , -1);
-    }
-    return solve(0, cost, time, cost.length);
-  }
+    int[][] dp = new int[cost.length + 1][cost.length + 1];
 
-  private int solve(int i, int[] cost, int[] time, int remain) {
-    if(remain <= 0) return 0;
-    if (i == cost.length) {
-      return (int) 1e9;
+    for (int i = 1; i <= cost.length; i++) {
+      dp[cost.length][i] = (int) 1e9;
     }
 
-    if(dp[i][remain] != -1) return dp[i][remain];
+    for (int i = cost.length - 1; i >= 0; i--) {
+      for (int remain = 0; remain <= cost.length; remain++) {
 
-    int p = cost[i] + solve(i+1 , cost , time , remain - 1 - time[i]);
-    int np = solve(i+1 , cost , time , remain);
+        dp[i][remain] = Math.min(
+            dp[i + 1][Math.max(0, remain - time[i] - 1)] +  cost[i],
+            dp[i + 1][remain]);
 
-    return dp[i][remain] = Math.min(p , np);
-
+      }
+    }
+    return dp[0][cost.length];
   }
+
 }
