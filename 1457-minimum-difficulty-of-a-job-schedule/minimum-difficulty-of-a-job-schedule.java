da994b786d8df63c70bae50ddb11dc1d27fa7165
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-15 17:04:18 -0400

    Time: 7 ms (98.94%) | Memory: 41.3 MB (72.47%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
@@ -1,39 +1,38 @@
 class Solution {
-    int[][] dp;
-    public int minDifficulty(int[] jobDifficulty, int d) {
-      int n = jobDifficulty.length;
-      if(n < d){
-        return -1;
-      }   
 
-      dp = new int[n][d + 1];
-      for(int[] arr : dp){
-        Arrays.fill(arr , -1);
-      }
-
-      return solve(0 , jobDifficulty , d);
+  public int minDifficulty(int[] jobDifficulty, int d) {
+    int n = jobDifficulty.length;
+    if (n < d) {
+      return -1;
     }
 
-    private int solve(int i , int[] jobDifficulty , int d){
-      if(i == jobDifficulty.length){
-        if(d != 0) return (int) 1e9;
-        return 0;
+    int[][] dp = new int[n + 1][d + 1];
+
+    for (int i = 0; i <= n; i++) {
+      for (int d1 = 0; d1 <= d; d1++) {
+        dp[i][d1] = (int) 1e9;
       }
-
-      if(d == 0) return (int) 1e9;
-
-      if(dp[i][d] != -1) return dp[i][d];
-
-      int curr = 0;
-      int res = (int) 1e9;
-
-      for(int j = i ; j < jobDifficulty.length ; j++){
-        curr = Math.max(curr , jobDifficulty[j]);
-        res = Math.min(res , curr + solve(j+1 , jobDifficulty , d-1));
-      }
-
-      return dp[i][d] = res;
-      
-
     }
+
+    dp[n][0] = 0;
+
+    for (int i = n - 1; i >= 0; i--) {
+      for (int d1 = 1; d1 <= d; d1++) {
+        int curr = 0;
+        int res = (int) 1e9;
+
+        for (int j = i; j < n; j++) {
+          curr = Math.max(curr, jobDifficulty[j]);
+          res = Math.min(
+              res,
+              curr + dp[j + 1][d1 - 1]);
+        }
+
+        dp[i][d1] = res;
+      }
+    }
+
+    return dp[0][d] == (int) 1e9 ? -1 : dp[0][d];
+  }
+
 }
