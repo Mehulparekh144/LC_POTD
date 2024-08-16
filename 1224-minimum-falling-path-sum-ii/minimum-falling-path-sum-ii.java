dd42ec79d25fd6194f3f3460cbb27e0f3e2746b6
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-16 17:51:08 -0400

    Time: 31 ms (53.31%) | Memory: 51.8 MB (12.16%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -7,34 +7,35 @@ class Solution {
       n = grid[0].length;
 
       dp = new int[m][n];
-      for(int[] row : dp) Arrays.fill(row , -1);
+
+      for(int j = 0 ; j < n ; j++){
+        dp[m-1][j] = grid[m-1][j];
+      }
+
+      for(int i = m-2 ; i >= 0 ; i--){
+        for(int j = n-1 ; j >= 0 ; j--){
+          int res = grid[i][j];
+          int min = Integer.MAX_VALUE;
+
+          for(int j1 = 0 ; j1 < n ; j1++){
+            if(j == j1) continue;
+            min = Math.min(min , dp[i+1][j1]);
+          }
+
+          res += min;
+          dp[i][j] = res;
+        }
+      }
 
       int res = Integer.MAX_VALUE;
       for(int j = 0 ; j < n ; j++){
         res = Math.min(
           res ,
-          solve(0 , j , grid)
+          dp[0][j]
         );
       }
 
       return res;
     }
 
-    private int solve(int i , int j , int[][] grid){
-      if(i == m-1){
-        return grid[i][j];
-      }
-
-      if(dp[i][j] != -1) return dp[i][j];
-
-      int res = grid[i][j];
-      int min = Integer.MAX_VALUE;
-      for(int j1 = 0 ; j1 < n ; j1++){
-        if(j == j1) continue;
-        min = Math.min(min , solve(i+1 , j1 , grid));
-      }
-
-      res += min;
-      return dp[i][j] = res;
-    }
 }
