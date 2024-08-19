c6c164d0d345d7168b84d6120954fb6dff177310
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-19 14:53:38 -0400

    Time: 5 ms (66.77%) | Memory: 121 MB (42.71%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
-        Set<Integer> s = new HashSet<>();
+      Set<Integer> s = new HashSet<>();
 
-        for(int[] triplet : triplets){
-            if(triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]){
-                continue;
-            }
+      for(int[] triplet : triplets){
+        if(triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) continue;
 
-            for(int i = 0 ; i < 3 ; i++){
-                if(triplet[i] == target[i]){
-                    s.add(i);
-                }
-            }
+        for(int i = 0 ; i < 3 ; i++){
+          if(triplet[i] == target[i]) s.add(i);
         }
+      }
 
-        return s.size() == target.length;
+      return s.size() == target.length;
     }
 }
