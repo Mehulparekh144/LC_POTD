ee962d5fef41909c9c9511ac320290e060f587c8
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-18 22:51:29 -0400

    Time: 52 ms (7.55%) | Memory: 55.8 MB (7.43%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,24 +1,31 @@
 class Solution {
-  int[][] dp;
-    public int minSteps(int n) {
-      if(n == 1) return 0;
-      dp = new int[n+1][n+1];
-      for(int[] row : dp) Arrays.fill(row , -1);
-      return 1 + solve(1 , 1 , n);
-    }
+  public int minSteps(int n) {
+    if (n == 1)
+      return 0;
+    int[][] dp = new int[n + 1][n + 1];
+    for (int[] row : dp)
+      Arrays.fill(row, (int) 1e9);
 
-    private int solve(int current , int clipboard , int n){
-      if(current == n){
-        return 0;
+    dp[1][0] = 0;
+    for (int current = 1; current <= n; current++) {
+      for (int clipboard = 0; clipboard <= current; clipboard++) {
+        // Copy All
+        if (current + current <= n) {
+          dp[current + current][current] = Math.min(dp[current + current][current], dp[current][clipboard] + 2);
+        }
+        // Paste
+        if (current + clipboard <= n) {
+          dp[current + clipboard][clipboard] = Math.min(dp[current + clipboard][clipboard], dp[current][clipboard] + 1);
+        }
       }
-
-      if(current > n) return (int) 1e9;
-
-      if(dp[current][clipboard] != -1) return dp[current][clipboard];
-
-      int copyAndPaste = 2 + solve(current*2 , current , n);
-      int paste = 1 + solve(current + clipboard , clipboard , n );
-      
-      return dp[current][clipboard]= Math.min(copyAndPaste , paste);
     }
+
+    int result = Integer.MAX_VALUE;
+    for (int clipboard = 1; clipboard <= n; clipboard++) {
+      result = Math.min(result, dp[n][clipboard]);
+    }
+
+    return result;
+  }
+
 }
