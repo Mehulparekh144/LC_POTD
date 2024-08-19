b5b020765b5f3963bf4e239c2c49d838bb2abb31
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-18 23:25:16 -0400

    Time: 31 ms (61.93%) | Memory: 45.8 MB (18.71%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,38 +1,37 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if(hand.length%groupSize != 0){
+      int n = hand.length;
+      if(n%groupSize != 0) return false;
+
+      Map<Integer,Integer> map = new HashMap<>();
+
+      for(int num : hand){
+        map.put(num , map.getOrDefault(num , 0) + 1);
+      }  
+
+      PriorityQueue<Integer> pq = new PriorityQueue<>();
+      for(int k : map.keySet()){
+        pq.offer(k);
+      }
+
+      while(!pq.isEmpty()){
+        int current = pq.peek();
+
+        for(int j = current ; j < current + groupSize ; j++){
+          if(!map.containsKey(j)){
             return false;
+          }
+
+          map.put(j , map.get(j) - 1 );
+
+          if(map.get(j) == 0){
+            if(j != pq.peek()) return false;
+            pq.remove(j);
+          }
+
         }
+      }
 
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int num : hand){
-            map.put(num , map.getOrDefault(num , 0 ) + 1);
-        }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int k : map.keySet()){
-            pq.offer(k);
-        }
-
-        while(!pq.isEmpty()){
-            int first = pq.peek();
-
-            for(int i = first ; i < first + groupSize ; i++){
-                if(!map.containsKey(i)){
-                    return false;
-                }
-
-                map.put(i , map.get(i) - 1);
-                if(map.get(i) == 0){
-                    if(i != pq.peek()){
-                        return false;
-                    }
-
-                    pq.remove(i);
-                }
-            }
-        }
-
-        return true;
+      return true;
     }
 }
