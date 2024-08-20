19aeefed7dac21f8a7769a1b0468b2700370ca38
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-19 22:48:23 -0400

    Time: 7 ms (60.19%) | Memory: 42.3 MB (75.97%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -1,26 +1,33 @@
 class Solution {
-    Integer[][] dp;
+    int[][] dp;
     public int stoneGameII(int[] piles) {
-        int totalStones = 0;
-        for(int pile : piles) totalStones += pile;
-
-        dp = new Integer[piles.length + 1][piles.length + 1];
-
-        return (totalStones + solve(0 , piles , 1))/2;
+      int total = Arrays.stream(piles).sum();
+      dp = new int[piles.length + 1][piles.length + 1];
+      for(int[] row : dp) Arrays.fill(row , -1);
+      // To get stones alice has 
+      // Total = A + B
+      // solve = A - B
+      // 2A/2 = A
+      return (total + solve(0 , piles , 1))/2;
     }
 
+    // The solve function gives difference of stones between Alice and Bob (A-B)
     private int solve(int i , int[] piles , int M){
-      if(i == piles.length) return 0;
+      if(i == piles.length){
+        return 0;
+      }
 
-      if(dp[i][M] != null) return dp[i][M];
+      if(dp[i][M] != -1 ) return dp[i][M];
 
       int total = Integer.MIN_VALUE;
       int stones = 0;
-      for(int X = 1 ; X <= 2*M ; X++){
-        if(i + X > piles.length) continue;
-
-        stones += piles[i + X - 1];
-        total = Math.max(total , stones - solve(i + X , piles , Math.max(M , X)));
+      for(int x = 1 ; x <= 2*M ; x++){
+        if(i + x > piles.length) continue;
+        stones += piles[i + x - 1];
+        total = Math.max(
+          total,
+          stones - solve(i + x , piles , Math.max(M , x))
+        );
       }
 
       return dp[i][M] = total;
