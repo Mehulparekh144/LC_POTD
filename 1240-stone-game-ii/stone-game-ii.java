73722ca1e5825b126a6be9e374640c2364dd22ac
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-19 22:52:59 -0400

    Time: 29 ms (12.38%) | Memory: 42.8 MB (53.64%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -1,35 +1,57 @@
 class Solution {
-    int[][] dp;
-    public int stoneGameII(int[] piles) {
-      int total = Arrays.stream(piles).sum();
-      dp = new int[piles.length + 1][piles.length + 1];
-      for(int[] row : dp) Arrays.fill(row , -1);
-      // To get stones alice has 
-      // Total = A + B
-      // solve = A - B
-      // 2A/2 = A
-      return (total + solve(0 , piles , 1))/2;
+  int[][] dp;
+
+  public int stoneGameII(int[] piles) {
+    int total = Arrays.stream(piles).sum();
+    int n = piles.length;
+    dp = new int[n + 1][n + 1];
+
+    for (int i = n - 1; i >= 0; i--) {
+      for (int M = n ; M >= 1; M--) {
+
+        int stones = 0;
+        dp[i][M] = Integer.MIN_VALUE;
+        for (int x = 1; x <= 2 * M; x++) {
+          if (i + x > piles.length)
+            continue;
+          stones += piles[i + x - 1];
+          dp[i][M] = Math.max(
+              dp[i][M],
+              stones - dp[i+x][Math.max(x , M)] );
+        }
+
+      }
     }
 
-    // The solve function gives difference of stones between Alice and Bob (A-B)
-    private int solve(int i , int[] piles , int M){
-      if(i == piles.length){
-        return 0;
-      }
+    return (total + dp[0][1])/2;
 
-      if(dp[i][M] != -1 ) return dp[i][M];
+    // To get stones alice has
+    // Total = A + B
+    // solve = A - B
+    // 2A/2 = A
+    // return (total + solve(0, piles, 1)) / 2;
+  }
 
-      int total = Integer.MIN_VALUE;
-      int stones = 0;
-      for(int x = 1 ; x <= 2*M ; x++){
-        if(i + x > piles.length) continue;
-        stones += piles[i + x - 1];
-        total = Math.max(
-          total,
-          stones - solve(i + x , piles , Math.max(M , x))
-        );
-      }
+  // The solve function gives difference of stones between Alice and Bob (A-B)
+  // private int solve(int i, int[] piles, int M) {
+  //   if (i == piles.length) {
+  //     return 0;
+  //   }
 
-      return dp[i][M] = total;
-    }
+  //   if (dp[i][M] != -1)
+  //     return dp[i][M];
+
+  //   int total = Integer.MIN_VALUE;
+  //   int stones = 0;
+  //   for (int x = 1; x <= 2 * M; x++) {
+  //     if (i + x > piles.length)
+  //       continue;
+  //     stones += piles[i + x - 1];
+  //     total = Math.max(
+  //         total,
+  //         stones - solve(i + x, piles, Math.max(M, x)));
+  //   }
+
+  //   return dp[i][M] = total;
+  // }
 }
