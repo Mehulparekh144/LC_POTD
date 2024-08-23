2cd7683fd7612d589fcf56b14421b5383553f0ce
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-08-23 16:06:40 -0400

    Time: 25 ms (38.96%) | Memory: 55.3 MB (60.04%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,46 +1,46 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        List<List<Integer>> adj = new ArrayList<>();
 
-        for(int i = 0 ; i < graph.length ; i++){
-          adj.add(new ArrayList<>());
-        }
+      // Reverse the edges
+      List<List<Integer>> adj = new ArrayList<>();
+      int[] indegree = new int[graph.length];
 
-        for(int i = 0 ; i < graph.length ; i++){
-          for(int child : graph[i]){
-            adj.get(child).add(i);
-          }
-        }
-
-        List<Integer> res = new ArrayList<>();
-        topo(adj , res , graph.length);
-        Collections.sort(res);
-        return res;
-    }
-
-    private void topo(List<List<Integer>> adj , List<Integer> res , int n){
-      int[] indegree = new int[n];
-      for(int i = 0 ; i < n ; i++){
-        for(int child : adj.get(i)){
-          indegree[child]++;
-        } 
+      for(int i = 0 ; i < graph.length ; i++){
+        adj.add(new ArrayList<>());
       }
 
+      for(int i = 0 ; i < graph.length ; i++){
+        for(int e : graph[i]){
+          adj.get(e).add(i);
+          indegree[i]++;
+        }
+      }
+
+      // Perform TOPO
+      List<Integer> res = new ArrayList<>();
+      topo(adj , indegree , res);
+      Collections.sort(res);
+
+      return res;
+    }
+
+    private void topo(List<List<Integer>> adj , int[] indegree  , List<Integer> res){
+      int n = adj.size();
       Queue<Integer> q = new LinkedList<>();
 
       for(int i = 0 ; i < n ; i++){
-        if(indegree[i] == 0) q.offer(i);
+        if(indegree[i] == 0){
+          q.offer(i);
+        }
       }
 
       while(!q.isEmpty()){
         int node = q.poll();
-        res.add(node);
+        res.addLast(node);
 
         for(int child : adj.get(node)){
           indegree[child]--;
-          if(indegree[child] == 0){
-            q.offer(child);
-          }
+          if(indegree[child] == 0) q.offer(child);
         }
       }
     }
