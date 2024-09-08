7c1d10b9bc64c867d0e73b07ba9f84aa9f9ab13d
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-08 15:28:57 -0400

    Time: 9 ms (100.00%) | Memory: 63.3 MB (100.00%) - LeetSync

--- 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.java
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.java
@@ -2,7 +2,7 @@ class Solution {
     public long findMaximumScore(List<Integer> nums) {
         long ans = 0;
         int curr = nums.getFirst();
-
+        // You will always jump to the number which is greater than the curr 
         for(int i = 1 ; i < nums.size() ; i++){
           ans += curr;
           curr = Math.max(curr , nums.get(i));
