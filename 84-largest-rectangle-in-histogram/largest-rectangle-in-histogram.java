3415006168f362c488f2d4e83fc5f33ae8ce17dc
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-07 21:33:20 -0400

    Time: 60 ms (78.01%) | Memory: 57.6 MB (43.05%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,46 +1,30 @@
 class Solution {
   public int largestRectangleArea(int[] nums) {
     int n = nums.length;
-
-    int[] nsl = new int[n];
-    int[] nsr = new int[n];
     Stack<Integer> st = new Stack<>();
-
-    for (int i = 0; i < n; i++) {
-
-      while (!st.isEmpty() && nums[st.peek()] >= nums[i]) {
-        st.pop();
-      }
-      if (st.isEmpty()) {
-        nsl[i] = -1;
-      } else {
-        nsl[i] = st.peek();
-      }
-
-      st.push(i);
-    }
-
-    st.clear();
-
-    for (int i = n - 1; i >= 0; i--) {
-
-      while (!st.isEmpty() && nums[st.peek()] >= nums[i]) {
-        st.pop();
-      }
-
-      if (st.isEmpty()) {
-        nsr[i] = n;
-      } else {
-        nsr[i] = st.peek();
-      }
-
-      st.push(i);
-    }
-
     int max = 0;
-    for (int i = 0; i < n; i++) {
-      int area = (nsr[i] - nsl[i] - 1) * nums[i];
-      max = Math.max(max, area);
+    for (int i = 0; i <= n; i++) {
+
+      while (!st.isEmpty() && (i == n || nums[st.peek()] >= nums[i])) {
+        // This condition means that we have found the next smaller element on the right side
+        // 2 1 5 6 now 2 comes so for 5 6, 2 is the next smaller
+        
+        int h = nums[st.pop()];
+        int w = 0;
+
+        if(st.isEmpty()){
+          // That means the current element is the nsl so whatever till this index is the width will be that
+          //eg. 5 1 1
+          // at index 2, st will be empty so 1 is the min height so all 3 elements will be considered
+          w = i;
+        } else{
+          w = i - st.peek() - 1;
+        }
+
+        max = Math.max(max , h*w);
+      }
+
+      st.push(i);
     }
 
     return max;
