f3c747f40258d1c86914ac2d60637f169e967284
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-09 09:56:33 -0400

    Time: 28 ms (66.05%) | Memory: 50.6 MB (82.59%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,32 +1,34 @@
 class Solution {
+  class Pair{
+    int x;
+    int y;
+    double dist;
 
-    class Pair{
-      int[] point;
-      double dist;
-
-      public Pair(int[] point , double dist){
-        this.point = point;
-        this.dist = dist;
-      }
+    public Pair(int x , int y , double dist){
+      this.x = x;
+      this.y = y;
+      this.dist = dist;
     }
-
+  }
     public int[][] kClosest(int[][] points, int k) {
+      PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> Double.compare(a.dist , b.dist));
+
+      for(int[] p : points){
+        int x = p[0] , y = p[1];
+        double dist = Math.sqrt(x*x + y*y);
+        pq.offer(new Pair(x , y , dist));
+      }
+
       int[][] res = new int[k][2];
-      PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> Double.compare(a.dist, b.dist));
-
-      for(int[] point : points){
-        int x = point[0];
-        int y = point[1];
-
-        double dis =  Math.sqrt(x*x + y*y);
-        pq.offer(new Pair(point , dis));
-      }   
 
       for(int i = 0 ; i < k && !pq.isEmpty() ; i++){
-        Pair p = pq.poll();
-        res[i] = p.point;
+        res[i] = new int[]{pq.peek().x , pq.peek().y};
+        pq.poll();
       }
 
       return res;
+
+
+        
     }
 }
