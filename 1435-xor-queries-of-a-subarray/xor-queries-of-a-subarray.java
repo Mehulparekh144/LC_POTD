6de17ebc086faac20cbe3b87902f41a308366097
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-12 22:23:57 -0400

    Time: 2 ms (100.00%) | Memory: 58 MB (33.03%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,17 +1,24 @@
 class Solution {
   public int[] xorQueries(int[] arr, int[][] queries) {
     int n = queries.length;
-    int[] ans = new int[queries.length];
+    int[] ans = new int[n];
+    int[] pref = new int[arr.length];
+    pref[0] = arr[0];
+
+    for(int i = 1 ; i < arr.length ; i++){
+      pref[i] = pref[i-1]^arr[i];
+    }
 
     for(int i = 0 ; i < n ; i++){
       int l = queries[i][0];
       int r = queries[i][1];
-      int xor = 0;
-      for(int j = l ; j <= r ; j++){
-        xor ^= arr[j];
-      }
 
-      ans[i] = xor;  
+      if(l == 0){
+        ans[i] = pref[r];
+      } else{
+        // Xoring with the previous element that has been xorred in prefix array will be nullifed 
+        ans[i] = pref[r] ^ pref[l-1];
+      }
     }
 
     return ans;
