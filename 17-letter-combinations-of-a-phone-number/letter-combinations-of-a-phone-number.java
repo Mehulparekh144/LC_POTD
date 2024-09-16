1533a409b98371ce38c6260e2e22c104723e8f3e
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-16 14:55:19 -0400

    Time: 5 ms (35.80%) | Memory: 42.2 MB (31.91%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,31 +1,36 @@
 class Solution {
-  public List<String> letterCombinations(String digits) {
-    if(digits.isEmpty()){
-      return new ArrayList<>();
-    }
-    Map<Character, String> map = new HashMap<>();
-    map.put('2', "abc");
-    map.put('3', "def");
-    map.put('4', "ghi");
-    map.put('5', "jkl");
-    map.put('6', "mno");
-    map.put('7', "pqrs");
-    map.put('8', "tuv");
-    map.put('9', "wxyz");
+    List<String> res;
+    Map<Character,List<Character>> map;
+    public List<String> letterCombinations(String digits) {
 
-    List<String> res = new ArrayList<>();
-    solve(0, digits, "", res, map);
-    return res;
-  }
+      res = new ArrayList<>();
+      if(digits.isEmpty()){
+        return res;
+      }
+      map = new HashMap<>();    
 
-  private void solve(int i, String digits, String path, List<String> res, Map<Character, String> map) {
-    if (path.length() == digits.length()) {
-      res.add(path.toString());
-      return;
+      map.put('2' , List.of('a' , 'b', 'c'));
+      map.put('3' , List.of('d' , 'e' , 'f'));
+      map.put('4' , List.of('g' , 'h' , 'i'));     
+      map.put('5' , List.of('j' , 'k' , 'l'));     
+      map.put('6' , List.of('m' , 'n' , 'o'));     
+      map.put('7' , List.of('p' , 'q' , 'r' , 's'));     
+      map.put('8' , List.of('t' , 'u' , 'v'));     
+      map.put('9' , List.of('w' , 'x' , 'y' , 'z'));     
+
+      solve(0 , digits , "");
+      return res;
     }
 
-    for (char c : map.get(digits.charAt(i)).toCharArray()) {
-      solve(i + 1, digits, path + c, res, map);
+    private void solve(int i , String digits , String curr){
+      if(i == digits.length()){
+        res.add(curr);
+        return;
+      }
+
+      char ch = digits.charAt(i);
+      for(char adj : map.get(ch)){
+        solve(i+1 , digits , curr + adj);
+      }
     }
-  }
 }
