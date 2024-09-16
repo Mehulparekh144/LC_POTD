66e66552570dcea1ec37e733344e5e98195b42d2
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-16 11:09:24 -0400

    Time: 9 ms (28.79%) | Memory: 57.3 MB (14.21%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,31 +1,33 @@
 class Solution {
+    List<List<String>> res;
+    int n;
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        solve(0 , res , new ArrayList<>() , s);
+        res = new ArrayList<>();
+        n = s.length();
+        solve(0 , s ,  new ArrayList<>());
         return res;
     }
 
-    private void solve(int i , List<List<String>> res , List<String> temp , String s){
-      if(i == s.length()){
+    private void solve(int i , String s , List<String> temp ){
+      if(i >= n){
         res.add(new ArrayList<>(temp));
         return;
       }
 
-      for(int j = i ; j < s.length() ; j++){
-        if(isPalindrome(s , i , j)){
+      for(int j = i ; j < n ; j++){
+        if(isPalindrome(i , j , s)){
           temp.add(s.substring(i , j+1));
-          solve(j+1 , res , temp , s);
+          solve(j+1 , s , temp);
           temp.removeLast();
         }
       }
     }
 
-    private boolean isPalindrome(String s , int i , int j){
+    private boolean isPalindrome(int i , int j , String s){
       while(i <= j){
         if(s.charAt(i) != s.charAt(j)){
           return false;
         }
-
         i++;
         j--;
       }
