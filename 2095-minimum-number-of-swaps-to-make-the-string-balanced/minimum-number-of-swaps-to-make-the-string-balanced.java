6f01c4f4d116794997036c06f43627334415c83f
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-20 17:40:46 -0400

    Time: 13 ms (92.25%) | Memory: 55.7 MB (31.54%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,18 +1,22 @@
 class Solution {
     public int minSwaps(String s) {
-        int close = 0;
-        int max = 0;
+        int open = 0;
+        int flips = 0;
 
         for(char c : s.toCharArray()){
           if(c == '['){
-            close--;
+            open++;
           } else{
-            close++;
+            open--;
           }
 
-          max = Math.max(max , close);
+          // If there are more closing than opening, then we have to make a swap, this will result in getting one opening bracket thus open = 1
+          if(open < 0){
+            flips++;
+            open = 1;
+          }
         }
 
-        return (max + 1)/2;
+        return flips;
     }
 }
