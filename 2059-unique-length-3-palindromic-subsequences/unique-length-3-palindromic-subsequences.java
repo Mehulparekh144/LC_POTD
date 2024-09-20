1d2d3a155cd50c45da2b24627d14ed51f5ed7a6b
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-20 17:04:17 -0400

    Time: 194 ms (61.06%) | Memory: 45.5 MB (40.43%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,31 +1,37 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-      int count = 0;
-      Set<Character> set = new HashSet<>();
-      for(char ch : s.toCharArray()){
-        set.add(ch);
-      }
-
-      for(char ch : set){
-        int leftMost = -1, rightMost = -1;
+        
+        int[][] indices = new int[26][2];
+        for(int[] row : indices) Arrays.fill(row , -1);
 
         for(int i = 0 ; i < s.length() ; i++){
-          if(s.charAt(i) == ch){
-            if(leftMost == -1) leftMost = i;
-            rightMost = i;
+          char ch = s.charAt(i);
+          int idx = ch - 'a';
+
+          if(indices[idx][0] == -1){
+            indices[idx][0] = i;
           }
+
+          indices[idx][1] = i;
         }
 
-        Set<Character> set2 = new HashSet<>();
-        for(int i = leftMost + 1 ; i <= rightMost-1 ; i++){
-          set2.add(s.charAt(i));
+        int count = 0;
+        // System.out.println(Arrays.deepToString(indices));
+
+        for(int i = 0 ; i < 26 ; i++){
+          int left = indices[i][0];
+          int right = indices[i][1];
+
+          if(left == -1) continue;
+
+          Set<Character> set2 = new HashSet<>();
+          for(int j = left + 1 ; j < right ; j++){
+            set2.add(s.charAt(j));
+          }
+
+          count += set2.size();
         }
 
-        count += set2.size();
-      }
-
-      return count;
-
-        
+        return count;
     }
 }
