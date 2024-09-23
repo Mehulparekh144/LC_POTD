5fa8093721a80977bf6eb09c8e244b16dc153217
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-22 22:29:23 -0400

    Time: 44 ms (65.99%) | Memory: 45.1 MB (75.17%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -1,27 +1,22 @@
 class Solution {
-    int[] dp;
     public int minExtraChar(String s, String[] dictionary) {
         Set<String> set = new HashSet<>();
-        dp = new int[s.length()];
-        Arrays.fill(dp , -1);
+        int[] dp = new int[s.length() + 1];
         for(String d : dictionary) set.add(d);
-        return solve(0 , s , set );
-    }
 
-    private int solve(int i , String s , Set<String> set){
-      if(i == s.length()) return 0;
 
-      if(dp[i] != -1) return dp[i];
+        for(int i = s.length() - 1 ; i >= 0 ; i--){
+          dp[i] = 1 + dp[i+1];
 
-      dp[i] = 1 + solve(i+1 , s , set);
-
-      for(int j = i + 1 ; j <= s.length() ; j++){
-        if(set.contains(s.substring(i , j))){
-          dp[i] =  Math.min(dp[i] , solve(j, s , set));
+          for(int j = i+1 ; j <= s.length() ; j++){
+            if(set.contains(s.substring(i , j))){
+              dp[i] = Math.min(dp[i] , dp[j]);
+            }
+          }
         }
-      }
 
-      return dp[i];
-
+        return dp[0];
     }
+
+   
 }
