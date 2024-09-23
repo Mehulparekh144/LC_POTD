b13a2443693505edcc4ac441cec7e69d538f68e3
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-23 11:45:42 -0400

    Time: 2 ms (42.22%) | Memory: 57 MB (73.79%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -25,11 +25,13 @@ class Solution {
           int idx = num - 1;
 
           if(nums[idx] < 0) continue;
+          // We mark it as -1 because it means that it has been visited
           nums[idx] *= -1;
         }
 
         for(int i = 0 ; i < n ; i++){
           if(nums[i] > 0){
+            // If its not visited or not minus one that means we weren't able to visit this number or this index since we get index as num - 1 if num is not there we won't be able to reach this index hence we return i + 1
             return i + 1;
           }
         }
