66d34ea758187fe78d7e7140f236d439c1e24797
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-09-24 12:12:37 -0400

    Time: 5 ms (37.39%) | Memory: 57.4 MB (66.97%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,42 +1,44 @@
 class Solution {
-    public int[] getSumAbsoluteDifferences(int[] nums) {
-        int n = nums.length;
-        int[] leftSum = new int[n];
-        int[] rightSum = new int[n];
+  public int[] getSumAbsoluteDifferences(int[] nums) {
+    int n = nums.length;
+    int[] prefix = new int[n];
 
-        leftSum[0] = 0;
+    prefix[0] = nums[0];
 
-        for(int i = 1 ; i < n ; i++){
-          leftSum[i] = leftSum[i-1] + nums[i-1];
-        }
+    for(int i = 1 ; i < n ; i++ ){
+      prefix[i] = prefix[i-1] + nums[i];
+    }
+    // In prefixSum , sum for left of i will be prefix[i] - arr[i];
+    // Sum for right of i will be prefix[n-1] - prefix[i]
 
-        rightSum[n-1] = 0;
-        for(int i = n-2 ; i >= 0 ; i--){
-          rightSum[i] = rightSum[i + 1] + nums[i+1];
-        }
+    int[] res = new int[n];
 
-        int[] res = new int[n];
-
-        for(int i = 0 ; i < n ; i++){
-          res[i] = i*nums[i] - leftSum[i] + rightSum[i] - nums[i]*(n-i-1);
-        }
-
-        return res;
+    for (int i = 0; i < n; i++) {
+      int leftSum = prefix[i] - nums[i];
+      int rightSum = prefix[n-1] - prefix[i];
+      res[i] = i * nums[i] - leftSum + rightSum - nums[i] * (n - i - 1);
     }
 
-      // for ith element in array
-        // Break it down to 3 parts.
-        // LEFT : 
-        // abs(nums[i] - nums[0]) + abs(nums[i] - nums[1]) + abs(nums[i] - nums[2]) ... + abs(nums[i] - nums[i-1])
+    return res;
+  }
 
-        // MIDDLE :
-        // abs(nums[i] - nums[i])
+  // for ith element in array
+  // Break it down to 3 parts.
+  // LEFT :
+  // abs(nums[i] - nums[0]) + abs(nums[i] - nums[1]) + abs(nums[i] - nums[2]) ...
+  // + abs(nums[i] - nums[i-1])
 
-        // RIGHT : 
-        // abs(nums[i] - nums[i+1]) + abs(nums[i] - nums[i+2]) ... + abs(nums[i] - nums[n-1])
+  // MIDDLE :
+  // abs(nums[i] - nums[i])
 
-        // Simplify all of them, since in left all numbers till i-1 are smaller than nums[i]. nums[i]*i - (nums[0] + ... + nums[i-1]);
+  // RIGHT :
+  // abs(nums[i] - nums[i+1]) + abs(nums[i] - nums[i+2]) ... + abs(nums[i] -
+  // nums[n-1])
 
-        // Middle will be 0 so doesn't matter
-        // Right again nums[i+1] onwards number will be greater so (nums[i+1] .. + nums[n-1] )  - nums[i]*(n- i - 1) 
+  // Simplify all of them, since in left all numbers till i-1 are smaller than
+  // nums[i]. nums[i]*i - (nums[0] + ... + nums[i-1]);
+
+  // Middle will be 0 so doesn't matter
+  // Right again nums[i+1] onwards number will be greater so (nums[i+1] .. +
+  // nums[n-1] ) - nums[i]*(n- i - 1)
 }
