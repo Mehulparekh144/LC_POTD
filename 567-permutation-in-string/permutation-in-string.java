04189f6724302f4fa15a97f39c79b9f1a33b8061
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2024-10-02 16:27:02 -0400

    Time: 5 ms (96.20%) | Memory: 42.3 MB (94.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,52 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()){
-          return false;
-        }
+        if(s2.length() < s1.length()) return false;
 
         int[] f1 = new int[26];
         int[] f2 = new int[26];
-        for(int i = 0 ; i < s1.length() ; i++){
-          f1[s1.charAt(i) - 'a']++;
-          f2[s2.charAt(i) - 'a']++;
-        }
 
-        int matches = 0;
-        for(int i = 0 ; i < 26 ; i++){
-          if(f1[i] == f2[i]){
-            matches++;
-          }
+        for(char c : s1.toCharArray()){
+          f1[c - 'a']++;
         }
 
         int l = 0;
 
-        for(int r = s1.length() ; r < s2.length() ; r++){
-          if(matches == 26) return true;
+        for(int r = 0 ; r < s2.length() ; r++){
+          char ch = s2.charAt(r);
+          int idx = ch - 'a';
 
-          char c1 = s2.charAt(r);
-          f2[c1 - 'a']++;
+          f2[idx]++;
 
-          if(f1[c1 - 'a'] == f2[c1 - 'a']){
-            matches++;
-          } else if(f1[c1 - 'a'] + 1 == f2[c1 - 'a']){
-            matches--;
+          if((r-l + 1) == s1.length() ){
+            if(Arrays.equals(f1, f2)){
+              return true;
+            } else{
+              f2[s2.charAt(l) - 'a']--;
+              l++;
+            }
           }
-
-          char c2 = s2.charAt(l);
-          f2[c2 - 'a']--;
-
-          if(f1[c2 - 'a'] == f2[c2 - 'a']){
-            matches++;
-          } else if(f1[c2 - 'a'] - 1 == f2[c2 - 'a']){
-            matches--;
-          }
-
-          l++;
-          
         }
 
-        return matches == 26;
-
-
+        return false;
     }
 }
