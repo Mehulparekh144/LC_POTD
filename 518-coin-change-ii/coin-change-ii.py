33709b77c982681623f48ed2fec3ca0000a59db5
Mehul Parekh <72097117+Mehulparekh144@users.noreply.github.com>
2025-07-24 21:20:56 -0700

    Time: 367 ms (38.41%) | Memory: 49.5 MB (38.06%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -2,19 +2,35 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
 
-        @cache
-        def dfs(i , target):
+        dp = [[0 for _ in  range(amount+1)] for _ in range(n+1)]
 
-            if i >= n:
-                if target == 0:
-                    return 1
-                return 0 
-            
-            np = dfs(i+1 , target)
-            p = 0
-            if target >= coins[i]:
-                p = dfs(i , target - coins[i])
-            
-            return np + p
+        dp[n][0] = 1
+
+        for i in range(n-1 , -1 , -1):
+            for target in range(amount+1):
+
+                np = dp[i+1][target]
+                p = 0
+                if target >= coins[i]:
+                    p = dp[i][target - coins[i]]
+                
+                dp[i][target] = np + p
         
-        return dfs(0 , amount)
+        return dp[0][amount]
+
+
+        # def dfs(i , target):
+
+        #     if i >= n:
+        #         if target == 0:
+        #             return 1
+        #         return 0 
+            
+        #     np = dfs(i+1 , target)
+        #     p = 0
+        #     if target >= coins[i]:
+        #         p = dfs(i , target - coins[i])
+            
+        #     return np + p
+        
+        # return dfs(0 , amount)
